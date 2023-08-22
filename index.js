793cb1fa68cf1f281defee658fa470ed26593471
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-08-22 09:07:11 +0545

    upper-Case

--- index.js
+++ index.js
@@ -10,15 +10,23 @@
 
 const http = require("http"); // nodejs inbuilt package
 const myModules = require("./myCustomModule");
+const upperCase = require("upper-case");
 
 http
   .createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" }); //html tag
     const date = myModules.myDate();
     const myTime = myModules.myTime();
+    const myName = upperCase.upperCase("nabin");
 
     res.write(
-      "<h1>Hello World, NaBiN</h1><p>" + date + "</p><p>" + myTime + "</p>"
+      "<h1>Hello World, " +
+        myName +
+        "</h1><p>" +
+        date +
+        "</p><p>" +
+        myTime +
+        "</p>"
     ); //Body
     res.end();
   })
