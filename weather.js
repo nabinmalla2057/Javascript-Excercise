428143e656c8324fc145df4c9affcc8c235e92b3
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-08-26 22:30:39 +0545

    weather rev

--- weather.js
+++ weather.js
@@ -1,27 +1,31 @@
 // // Weather API  using package axios and open source weather
 
-// const axios = require("axios");
-// const moment = require("moment");
+const moment = require("moment");
+const axios = require("axios");
 
-// async function getWeather() {
-//   try {
-//     const response = await axios.get(
-//       "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
-//     );
-//     const { data } = response;
-//     const currentDateAndTime = new Date().toISOString();
-//     console.log({ currentDateAndTime });
-//     const { daily } = data;
-//     const { sunrise, sunset } = daily;
-//     const sr = moment(sunrise.toISOString()).format("LLLL");
-//     const ss = moment(sunset.toISOString()).format("LLLL");
-//     return { sunrise: sr, sunset: ss };
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+async function getWeather() {
+  try {
+    const response = await axios.get(
+      "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
+    );
+    const { data } = response;
+    const { daily } = data;
+    const { sunrise, sunset } = daily;
+    const currentDateAndTime = new Date().toISOString();
 
-// async function result() {
-//   const sunStatus = await getWeather();
-//   console.log({ sunStatus });
-// }
+    console.log({ currentDateAndTime });
+
+    const sr = moment(sunrise.toString()).format("LLLL");
+    const ss = moment(sunset.toString()).format("LLLL");
+
+    return { sunrise: sr, sunset: ss };
+  } catch (error) {
+    console.error(error);
+  }
+}
+async function result() {
+  const sunStatus = await getWeather();
+  console.log({ sunStatus });
+}
+
+result();
